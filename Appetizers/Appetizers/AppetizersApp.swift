8c9a9dda431fce6c63af099241a93770ef000255
Kody Buss <kody.buss@gmail.com>
2024-02-23 23:33:43 -0600

    create project

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Kody Buss on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
